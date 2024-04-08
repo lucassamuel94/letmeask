e4da38b660196fcaf3b7caaee5e37fb6ab46dfd8
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-04-08 12:48:24 -0300

    metadata: update config site

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -1,18 +1,6 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: 'Boilerplate Next.js',
-  description:
-    'Componentes lindamente projetados construídos com Radix UI e Tailwind CSS.',
-  mainNav: [
-    {
-      title: 'Home',
-      href: '/'
-    }
-  ],
-  links: {
-    twitter: 'https://twitter.com/shadcn',
-    github: 'https://github.com/shadcn/ui',
-    docs: 'https://ui.shadcn.com'
-  }
+  name: 'Letmeask',
+  description: 'Projeto da NLW Rocketseat'
 }
